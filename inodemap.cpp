1c7145ab63de3a761bf7009cbdda10ba7b8060ca
Wepdekr <1013006824@qq.com>
2020-12-28 17:53:16 +0800

    Added inodemap.cpp

---
+++ inodemap.cpp
@@ -0,0 +1,21 @@
+#include "include/inodemap.h"
+
+using namespace std;
+
+InodeMap::InodeMap(const ByteArray& d)
+{
+    //Function body here
+}
+//Notes here
+
+ByteArray InodeMap::dataExport()
+{
+    //Function body here
+}
+//Notes here
+
+LBA_t InodeMap::queryLBA(const inum_t i)
+{
+    //Function body here
+}
+//Notes here
