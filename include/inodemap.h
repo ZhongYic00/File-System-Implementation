50e385ee046936c838732865fccb4baa819ae151
zhongyic00 <zhong200234@outlook.com>
2020-12-29 14:47:11 +0800

    supplement functions

--- include/inodemap.h
+++ include/inodemap.h
@@ -6,6 +6,8 @@ class InodeMap {
 public:
     InodeMap(const ByteArray& d);
     ByteArray dataExport() const;
-    LBA_t queryLBA(const inum_t i) const;
+    LBA_t queryLBA(const inum_t& i) const;
+    void updateLBA(const inum_t& i, const LBA_t& addr); //update record if i exists, else insert the record
+    void remove(const inum_t& i); //remove the record
 };
 #endif
