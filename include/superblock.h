bbc92ddd216e5b9ccd231cfd6d615aff8ffaa0b2
zhongyic00 <zhong200234@outlook.com>
2020-12-29 21:52:15 +0800

    supplement function declaration

--- include/superblock.h
+++ include/superblock.h
@@ -4,15 +4,16 @@
 #include "fsnode.h"
 class Superblock {
 public:
-    LBA_t extentTreeLBA() const;
-    LBA_t extentTreeBakLBA() const;
-    LBA_t extentTreeRendLBA() const;
-    LBA_t freeInumPoolLBA() const;
-    LBA_t inodeMapLBA() const;
-    void updateExtentTreeLBA(const LBA_t& lba);
-    void updateExtentTreeRendLBA(const LBA_t& lba);
-    void updateInodeMapLBA(const LBA_t& lba);
-    inum_t inumCnt() const;
-    bool invalid() const;
+    inline LBA_t root() const;
+    inline LBA_t extentTreeLBA() const;
+    inline LBA_t extentTreeBakLBA() const;
+    inline LBA_t extentTreeRendLBA() const;
+    inline LBA_t freeInumPoolLBA() const;
+    inline LBA_t inodeMapLBA() const;
+    inline void updateExtentTreeLBA(const LBA_t& lba);
+    inline void updateExtentTreeRendLBA(const LBA_t& lba);
+    inline void updateInodeMapLBA(const LBA_t& lba);
+    inline inum_t inumCnt() const;
+    inline bool invalid() const;
 };
 #endif
