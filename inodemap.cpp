fe0231655bbfe407e649d206928384d7cc02b2ee
Wepdekr <1013006824@qq.com>
2020-12-29 16:07:29 +0800

    Fixed some bugs in Inodemap

--- inodemap.cpp
+++ inodemap.cpp
@@ -4,17 +4,29 @@ using namespace std;
 
 InodeMap::InodeMap(const ByteArray& d)
 {
-    
+    //Function body here
 }
 //Notes here
 
-ByteArray InodeMap::dataExport()
+ByteArray InodeMap::dataExport() const
 {
     //Function body here
 }
 //Notes here
 
-LBA_t InodeMap::queryLBA(const inum_t i)
+LBA_t InodeMap::queryLBA(const inum_t& i) const
+{
+    //Function body here
+}
+//Notes here
+
+void InodeMap::updateLBA(const inum_t &i, const LBA_t &addr)
+{
+    //Function body here
+}
+//Notes here
+
+void InodeMap::remove(const inum_t &i)
 {
     //Function body here
 }
