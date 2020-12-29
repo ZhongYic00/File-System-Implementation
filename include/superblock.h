d78217f493d84fe915d8746cf26fbdbfe02dbe78
Wepdekr <1013006824@qq.com>
2020-12-29 22:32:35 +0800

    Supplement function body

--- include/superblock.h
+++ include/superblock.h
@@ -14,6 +14,28 @@ public:
     inline void updateExtentTreeRendLBA(const LBA_t& lba);
     inline void updateInodeMapLBA(const LBA_t& lba);
     inline inum_t inumCnt() const;
-    inline bool invalid() const;
+    inline bool isValid() const;
+
+private:
+    LBA_t _root;
+    LBA_t _extentTreeLBA;
+    LBA_t _extentTreeBakLBA;
+    LBA_t _extentTreeRendLBA;
+    LBA_t _freeInumPoolLBA;
+    LBA_t _inodeMapLBA;
+    inum_t _inumCnt;
+    bool valid;
 };
+
+LBA_t Superblock::root() const { return _root; }
+LBA_t Superblock::extentTreeLBA() const { return _extentTreeLBA; }
+LBA_t Superblock::extentTreeBakLBA() const { return _extentTreeBakLBA; }
+LBA_t Superblock::extentTreeRendLBA() const { return _extentTreeRendLBA; }
+LBA_t Superblock::freeInumPoolLBA() const { return _freeInumPoolLBA; }
+LBA_t Superblock::inodeMapLBA() const { return _inodeMapLBA; }
+void Superblock::updateExtentTreeLBA(const LBA_t& lba) { _extentTreeLBA = lba; }
+void Superblock::updateExtentTreeRendLBA(const LBA_t& lba) { _extentTreeRendLBA = lba; }
+void Superblock::updateInodeMapLBA(const LBA_t& lba) { _inodeMapLBA = lba; }
+inum_t Superblock::inumCnt() const { return _inumCnt; }
+bool Superblock::isValid() const { return valid; }
 #endif
